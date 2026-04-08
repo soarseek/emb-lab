1980860fda5ab62cc253a1630444ca81d6fea221
TangHaorancom <2164715486@qq.com>
2026-04-08 11:57:45 +0800

    LED闪烁

---
+++ LED闪烁/code/main.c
@@ -0,0 +1,22 @@
+#include "delay.h"
+#include "stm32f10x.h"
+
+int main(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	
+	while(1)
+	{
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+		Delay_ms(500);
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+		Delay_ms(500);
+	}
+}
