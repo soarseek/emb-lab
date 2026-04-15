711232e19778f9af596607efa050b9b90afe4990
TangHaorancom <2164715486@qq.com>
2026-04-15 10:51:43 +0800

    a=update 按键控制

--- 按键控制/code/main.c
+++ 按键控制/code/main.c
@@ -4,6 +4,8 @@
 #include "Key.h"
 
 uint8_t KeyNum;
+uint8_t Key1_Count = 0;
+uint8_t Key2_Count = 0;
 
 int main(void)
 {
@@ -19,11 +21,27 @@ int main(void)
 		KeyNum = Key_GetNum();
 		if(KeyNum == 1)
 		{
-			LED_Off(LED_0);
+			Key1_Count++;
+			if(Key1_Count % 2 == 1)
+			{
+				LED_On(LED_0);
+			}
+			else
+			{
+				LED_Off(LED_0);
+			}
 		}
 		else if(KeyNum == 2)
 		{
-			LED_On(LED_0);
+			Key2_Count++;
+			if(Key2_Count % 2 == 1)
+			{
+				LED_On(LED_3);
+			}
+			else
+			{
+				LED_Off(LED_3);
+			}
 		}
 		Delay_ms(10);
 	}
